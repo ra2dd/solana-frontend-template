6a0115d33c5d9e3424ec5d55a657f370bbaaa52f
ra2dd <radoslawpryw@gmail.com>
2023-11-21 20:59:03 +0100

    add tailwind to project

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,11 +18,13 @@ const Home: NextPage = (props) => {
       <AppBar />
 
       <div className={styles.content}>
+        <form className="bg-teal-900 p-12 rounded max-w-4xl shadow-md">
+          <h2 className="text-lg mb-8">Action description</h2>
+          <input className="leading6 bg-teal-700 py-2 pl-1 mb-6 rounded-md w-64 focus:ring-2 focus:ring-teal-500 focus:outline-none" type="text" placeholder="Label" />
+          <button type="submit" className="px-4 py-2 font-bold rounded-md bg-blue-700 text-teal-100">Submit</button>
+        </form>
+
         <BalanceDisplay />
-        <div>
-          <p>Test</p>
-          <button>Submit</button>
-        </div>
       </div>
 
       <div></div>
